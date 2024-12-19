45af85abb53af0e05c54fadf0ffbb903a11c472b
Eduardo Llamas <2411630720101@ingenieria.usac.edu.gt>
2024-12-18 22:32:46 -0600

    [UPDATE] - Módulo de atención al cliente

---
+++ backend/controllers/AtencionCliente.mjs
@@ -0,0 +1,4 @@
+// Propósito: Controlador de ejemplo para mostrar el funcionamiento de la API
+import configurations from "../utils/configurations.mjs";
+import db from "../utils/db_connection.mjs";
+import nodemailer from 'nodemailer';
