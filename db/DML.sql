3a7f781858fc281933fb08b83e193a232362ab79
Steven González <stevengonzalez088@gmail.com>
2024-12-20 00:44:47 -0600

    [UPDATE] Se actualiza DML con info de servicios

--- db/DML.sql
+++ db/DML.sql
@@ -12,3 +12,10 @@ VALUES
 ('admin', '$2a$10$sLiWO.iG/Qlyjk4hLGWsYuygk9u2VqcVrI6p6Z74sCKfP8B1gqW3e', 'John', 'Doe', 'jdoe@example.com', 'A', 1, 'admin', 'admin'),
 ('tiky', '$2a$10$sLiWO.iG/Qlyjk4hLGWsYuygk9u2VqcVrI6p6Z74sCKfP8B1gqW3e', 'Alice', 'Smith', 'asmith@example.com', 'A', 2, 'admin', 'admin'),
 ('bcarter', '$2a$10$sLiWO.iG/Qlyjk4hLGWsYuygk9u2VqcVrI6p6Z74sCKfP8B1gqW3e', 'Bob', 'Carter', 'bcarter@example.com', 'I', 3, 'admin', 'admin');
+
+-- Insertar datos en la tabla SERVICIO
+insert into SERVICIO (NOMBRE, PROVEEDOR, MONTO, TIPO) values ('Steven Gonzalez', 'Empresa de Agua de Guatemala', 250.00, '1');
+insert into SERVICIO (NOMBRE, PROVEEDOR, MONTO, TIPO) values ('Robin Buezo', 'Empresa Electrica de Guatemala', 300.00, '2');
+insert into SERVICIO (NOMBRE, PROVEEDOR, MONTO, TIPO) values ('Danny Flow', 'Tigo Guatemala', 350.00, '4');
+insert into SERVICIO (NOMBRE, PROVEEDOR, MONTO, TIPO) values ('Juan Pablo Gonzalez', 'Claro Guatemala', 400.00, '3');
+insert into SERVICIO (NOMBRE, PROVEEDOR, MONTO, TIPO) values ('Eduardo Llamas', 'Claro Guatemala', 450.00, '3');
