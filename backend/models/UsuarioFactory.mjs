942bb93f1a9e7cd92f126b0c9115f0c71000b70c
robinbuezo11 <robinbuezo11@gmail.com>
2024-12-22 14:46:26 -0600

    [ADD] Se agrega funcionalidad de actualizar y eliminar informacion de administradores

--- backend/models/UsuarioFactory.mjs
+++ backend/models/UsuarioFactory.mjs
@@ -1,14 +1,17 @@
 import { Administrador } from "../models/Administrador.mjs";
 
 class UsuarioFactory {
-    static crearUsuario(tipo, cui, nombre, apellido, telefono, correo, params) {
+    static crearUsuario(tipo, id_usuario, usuario, cui, nombre, apellido, telefono, correo, params) {
         switch (tipo) {
             // case 'cliente':
             //     return new Cliente();
             // case 'supervisor':
             //     return new Supervisor();
             case 'admin':
+                console.log(params);
                 return new Administrador(
+                    id_usuario,
+                    usuario,
                     nombre, 
                     apellido,
                     telefono,
