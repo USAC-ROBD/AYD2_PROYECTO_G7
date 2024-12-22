b346f5b61a7acedcf26c2cdd204fb1ca39bb6b80
Steven González <stevengonzalez088@gmail.com>
2024-12-22 13:49:33 -0600

    [ADD] Archivos para dockerización

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // Esto asegura que las rutas sean relativas
   plugins: [react()],
   server: {
     port: parseInt(process.env.VITE_PORT) || 5173,
-  }
+  },
 })
