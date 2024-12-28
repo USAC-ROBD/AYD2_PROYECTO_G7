20e2beb557763ec66c623fcc8e73c46445f03c59
Steven González <stevengonzalez088@gmail.com>
2024-12-28 01:16:25 -0600

    [ADD] Test e3e para retiros

---
+++ front-end/cypress/e2e/retiros.cy.js
@@ -0,0 +1,19 @@
+describe('Pago de servicios', () => {
+    it('debería realizar el pago de un servicio pero el servicio ya esta pagado', () => {
+        cy.visit('/'); // Visita la raíz del proyecto
+        cy.contains('Login'); // Busca texto específico en la página
+        cy.get('input[name="username"]').type('tiky');
+        cy.get('input[name="password"]').type('admin');
+        cy.get('button[type="submit"]').click();
+        cy.url().should('include', '/menu');
+        cy.contains('Menú Principal');
+        cy.get('button[name="modulo-retiro"]').click();
+        cy.contains('Retiro de Cuenta');
+        cy.get('input[name="origenCuenta"]').type('8285710546');
+        cy.get('button[name="consultar"]').click();
+        cy.get('input[name="montoRetirar"]').type('100');
+        cy.get('button[name="retirar"]').click();
+        cy.get('button[class="swal2-confirm swal2-styled"]').click();
+        cy.contains('Confirmación de Retiro');
+    });
+});
