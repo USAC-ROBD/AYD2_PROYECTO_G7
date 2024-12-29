767f63ed50051b5194d133ef9a9108d714a75afd
Steven González <stevengonzalez088@gmail.com>
2024-12-28 21:21:48 -0600

    [UPDATE] Se actualiza politica de acceso para puerto 4000 en terraform

--- backend/controllers/auth.mjs
+++ backend/controllers/auth.mjs
@@ -45,7 +45,7 @@ const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: false,
       secure: false,
-      sameSite: 'none',
+      sameSite: 'strict',
       maxAge: 3600000,
     });
 
